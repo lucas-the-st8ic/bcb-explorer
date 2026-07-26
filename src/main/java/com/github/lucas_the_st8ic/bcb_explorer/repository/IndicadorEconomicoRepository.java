6e8b977136d24bfc71a154044c1faf5680a37267
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-26 20:32:33 -0300

    Update IndicadorEconomicoRepository.java

--- src/main/java/com/github/lucas_the_st8ic/bcb_explorer/repository/IndicadorEconomicoRepository.java
+++ src/main/java/com/github/lucas_the_st8ic/bcb_explorer/repository/IndicadorEconomicoRepository.java
@@ -4,5 +4,5 @@ import com.github.lucas_the_st8ic.bcb_explorer.entity.IndicadorEconomico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndicadorEconomicoRepository extends JpaRepository<IndicadorEconomico, Long> {
-    
+
 }
